d89a3236dd4699b635a78f0fe9c920e37a7e589d
jalyseh <hammettjadyn@gmail.com>
2020-10-15 22:15:50 -0400

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
-  title = 'calcdisocunt';
+  title= "Handy Dandy Discount Calculator";
+  discountAmount= 0;
+  finalPrice=0;
+
+  calculate(price,discount){
+    this.discountAmount = (discount / 100) * price;
+    this.finalPrice = price - this.discountAmount;
+    return this.finalPrice;
+  }
 }
