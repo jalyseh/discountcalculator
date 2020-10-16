2c3717bb95dd3ee9b8d9d681afc493d5bb79fb04
jalyseh <hammettjadyn@gmail.com>
2020-10-15 22:34:03 -0400

    thirs commit

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/calcdisocunt'));
+app.use(express.static('./dist/calcdiscount'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/calcdisocunt' }
+  res.sendFile('index.html', { root: 'dist/calcdiscount' }
   );
 });
 
